4e538582c90da35ef2f8344754b11bdb07b18cb4
Jarvis <mengda636431@gmail.com>
2026-01-22 22:17:57 +0800

    fix: lint检测忽略自身

--- eslint.config.js
+++ eslint.config.js
@@ -8,6 +8,18 @@ module.exports = [
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    files: ["eslint.config.js", "prettier.config.js"],
+    languageOptions: {
+      globals: {
+        module: "readonly",
+        require: "readonly",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  {
     files: ["**/*.ts"],
     rules: {
       "@typescript-eslint/no-unused-vars": [
